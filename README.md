2f66d48a875419885cee382a66071d47acf25a28
Rodrigo Morais <morais.rm@gmail.com>
2013-05-31 19:46:38 -0300

    Criando repositório.

---
+++ README.md
@@ -0,0 +1,4 @@
+UDP Client
+===========================
+
+Aplicativo que simula um cliente em uma troca de mensagens utilizando socket e o protocolo UDP.
---
+++ UDPClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDPClient
+{
+    class Program
+    {
+
+        private static UdpClient client;
+        
+        static void Main(string[] args)
+        {
+
+            client = new UdpClient("127.0.0.1", 9999);
+            Console.WriteLine("Client connection");
+
+            while (true)
+            {
+                Console.Write(">> ");
+                String message = Console.ReadLine();
+                send(message);
+            }
+
+        }
+
+        private static void send(string message)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            client.Send(data, data.Length);
+        }
+    }
+}
